972a9791dde0fb3920f252210b9edf7583d8a39c
Shiva Kumar Allam <shivaallam17@gmail.com>
2023-08-22 14:43:21 -0500

    Added line

--- PracticeGithub/src/main/java/com/example/PracticeGithub/PracticeGithubApplication.java
+++ PracticeGithub/src/main/java/com/example/PracticeGithub/PracticeGithubApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PracticeGithubApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PracticeGithubApplication.class, args);
+		System.out.println("Added line");
 	}
 
 }
